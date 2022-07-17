a6a6b32e863f10a414712eb605124bcc8f4d702f
Bruno Finger <bruno.k.finger@gmail.com>
2022-07-17 20:05:43 +0200

    Button

---
+++ index.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import { useBem } from "@protolane/bem";
+
+export type IButtonProps = {
+    onClick: () => void;
+    className?: string;
+}
+
+export default function Button({onClick, className}: IButtonProps) {
+    const bem = useBem('button', 'protolane');
+    return <button {...bem(null, null, className)} onClick={() => onClick?.()}>Hello world</button>
+}
